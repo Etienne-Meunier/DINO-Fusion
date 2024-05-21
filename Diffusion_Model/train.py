3470d8e66f84fdd677092a2ac57e7ec9ca2abfcf
emeunier <etiennemeunier@live.fr>
2024-05-21 15:07:23 +0200

    Dataloader webdataset

--- Diffusion_Model/train.py
+++ Diffusion_Model/train.py
@@ -1,7 +1,7 @@
 from DiffusionModel import DiffusionModel
 from configs.base_config import TrainingConfig
 from torchvision import transforms
-#from datasets import load_dataset
+from utils import get_dataloader
 from tqdm import tqdm
 import torch 
 from utils import save_images
@@ -11,32 +11,8 @@ def main() :
     # Load config
     config = TrainingConfig()
     
-    #########################################################
-    ## Load dataset
-    #config.dataset_name = "huggan/smithsonian_butterflies_subset"
-    #dataset = load_dataset(config.dataset_name, split="train")
-
-
-    #preprocess = transforms.Compose(
-    #    [
-    #        transforms.Resize((config.image_size, config.image_size)),
-    #        transforms.RandomHorizontalFlip(),
-    #        transforms.ToTensor(),
-    #        transforms.Normalize([0.5], [0.5]),
-    #    ]
-    #)
-
-    #def transform(examples):
-    #    images = [preprocess(image.convert("RGB")) for image in examples["image"]]
-    #    return {"images": images}
-
-    #dataset.set_transform(transform)
-    #train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
-    #########################################################
-    
-    dataset = np.load(config.data_file)[:, np.append(np.arange(0,35),70) ,:,:]
-    mask = None #1. * (dataset[0,-1,:,:]!=-1.0557332)
-    train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
+    mask = None#TODO:load mask here
+    train_dataloader = get_dataloader(config.data_file, batch_size=config.train_batch_size)
     
     # Load Model 
     diffusion = DiffusionModel(config)
