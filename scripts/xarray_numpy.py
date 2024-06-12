3de6981ea659fc416c1fdd446d0e9df8203aaa5a
Etienne-Meunier <etiennemeunier@live.fr>
2024-06-12 06:42:20 +0200

    fix script

--- scripts/xarray_numpy.py
+++ scripts/xarray_numpy.py
@@ -5,8 +5,9 @@ from tqdm import tqdm
 
 
 restarts = glob('/gpfsstore/rech/omr/uym68qx/nemo_output/DINO/Dinoffusion/1_4degree/restart*')
-save_path = '/gpfsstore/rech/gzi/ufk69pe/DINO-Fusion-Data/1_4_degree/'
+save_path = '/gpfsstore/rech/gzi/ufk69pe/DINO-Fusion-Data/1_4_degree'
 
+f = open(save_path + '_files.txt')
 idx = 0
 for restart in restarts :
     try :
@@ -16,13 +17,20 @@ for restart in restarts :
         print(restart, e)
         continue
 
-    toce = data_TS.toce_inst.values
-    soce = data_TS.soce_inst.values
-    ssh = data_SSH.ssh_inst.values
-    for i in tqdm(range(len(toce))) :
-        np.save(save_path + f'toce_{idx}.npy', toce[i])
-        np.save(save_path + f'soce_{idx}.npy', soce[i])
-        np.save(save_path + f'ssh_{idx}.npy',  ssh[i])
-        idx += 1
-    del data_TS, data_SSH, toce, soce, ssh
+    data = {}
+    infos = {}
+    data['toce.npy'] = data_TS.toce_inst.values
+    data['soce.npy'] = data_TS.soce_inst.values
+    data['ssh.npy'] = data_SSH.ssh_inst.values
+    for i in tqdm(range(len(data['toce']))) :
+        for key in data.keys() :
+            name=f'{idx:05d}.{key}'    
+            np.save(save_path + name, data[key][i])
+            f.writelines(name+'\n')
+            idx += 1
+    del data_TS, data_SSH, data
+f.close()
 
+d = data['toce.npy']
+x_mean = np.nanmean(d, axis=(0,2,3), keepdims=False)
+x_std  = np.nanstd(d, axis=(0,2,3), keepdims=False)
