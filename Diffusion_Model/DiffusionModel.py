fixing normalisation

Diffusion_Model/DiffusionModel.py
@@ -31,7 +31,7 @@ class DiffusionModel(nn.Module) :
                                   gradient_accumulation_steps=self.config.gradient_accumulation_steps, 
                                   log_with=self.config.logger)
         
-        if accelerator.is_main_process:
+        if (self.config.logger == 'wandb') and accelerator.is_main_process:
             accelerator.init_trackers('dino-fusion', config=vars(self.config))
             accelerator.trackers[0].run.log_code("../")
         if self.config.output_dir == 'wandb' : 

Diffusion_Model/utils.py
@@ -82,7 +82,8 @@ class TransformFields :
 
 
         def stride_concat(self, sample) : 
-            return np.concatenate((sample['soce'][::self.step], sample['toce'][::self.step], sample['ssh']), axis=0)
+            # Remove last dimension as it's just 0
+            return np.concatenate((sample['soce'][:-1:self.step], sample['toce'][:-1:self.step], sample['ssh']), axis=0)
         
         def un_stride_concat(self, data) : 
             assert self.step == 2, 'Only works for step=2 for now'