75d514f1ce22a1130bcdcb7cf941fa57fefe067b
Matt Archer <ma595@cam.ac.uk>
2025-11-25 12:44:54 +0000

    Pass nav_lat and nav_lon to data

--- Results/analysis_scripts/Paper-Figures/ICLRw-tables.py
+++ Results/analysis_scripts/Paper-Figures/ICLRw-tables.py
@@ -81,6 +81,13 @@ for gen in ['no_constraint', 'constraint', 'data'] :
         for bi, b in  enumerate(batch_raw[field]) :
             data = file_mask_LR.e3t_0.copy()
             data[:] = b
+            data = data.assign_coords(
+                {
+                    "nav_lat": (("nav_lat", "nav_lon"), file_mask_LR["nav_lat"].values),
+                    "nav_lon": (("nav_lat", "nav_lon"), file_mask_LR["nav_lon"].values),
+                }
+            )
+
             base = {'source' : gen, 'index' : bi, 'field' : field}
             for key, f in metrics.items() :
                 stats.append(base | {'metric' : key , 'value' : f(data, file_mask_LR).item()})
